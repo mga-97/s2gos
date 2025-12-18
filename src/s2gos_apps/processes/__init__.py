1fcb242de661161f5fb0fd9d344fa7a3098f1a3e
nicolae marton <nicolae.marton@gmail.com>
2025-12-18 09:44:53 +0100

    Add mock upscaling process

--- src/s2gos_apps/processes/__init__.py
+++ src/s2gos_apps/processes/__init__.py
@@ -2,6 +2,7 @@ from s2gos_apps.registry import registry
 
 from . import frascati, gobabeb, kairouan, pisa, pnp
 from .common import generation, simulation
+from .upscaling import upscaling
 
 __all__ = [
     "registry",
@@ -10,6 +11,7 @@ __all__ = [
     "kairouan",
     "pisa",
     "pnp",
+    "upscaling",
     "generation",
     "simulation",
 ]
